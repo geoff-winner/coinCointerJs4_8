9142f19412d5ca371c922226b9a6d2255291c77d
geoff & james <student@epicodus.com>
2015-04-06 17:00:39 -0700

    add javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,58 @@
-var leapYear = function(year) {
-    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+// var leapYear = function(year) {
+//     if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+//
+// $(document).ready(function() {
+//     $("form#leap-year").submit(function(event) {
+//         var year = parseInt($("input#year").val());
+//         var result = leapYear(year);
+//
+//         $(".year").text(year);
+//         $(".not").text("");
+//         if (!result) {
+//             $(".not").text("not");
+//         }
+//
+//         $("#result").show();
+//         event.preventDefault();
+//     });
+// });
+
+var queenAttack = function(attack, victim) {
+    var x = attack[0];
+    var y = attack[1];
+    var a = victim[0];
+    var b = victim[1];
+
+    var attacksum = attack[0] + attack[1];
+    var victimsum = victim[0] + victim[1];
+
+    var attacksub = attack[0] - attack[1];
+    var victimsub = victim[0] - victim[1];
+
+    //queen [x,y]
+    //victim [a,b]
+    if (x === a || y === b || attacksum === victimsum || attacksub === victimsub) {
         return true;
-    } else {
+    }   else {
         return false;
     }
 };
 
 $(document).ready(function() {
-    $("form#leap-year").submit(function(event) {
-        var year = parseInt($("input#year").val());
-        var result = leapYear(year);
+    $("form#queen-attack").submit(function(event) {
+        var x = parseInt($('input#queen_x').val());
+        var y = parseInt($('input#queen_y').val());
+        var a = parseInt($('input#victim_a').val());
+        var b = parseInt($('input#victim_b').val());
+        var result = queenAttack(attack, victim);
 
-        $(".year").text(year);
+        $(".attack").text(attack);
+        $(".victim").text(victim);
         $(".not").text("");
         if (!result) {
             $(".not").text("not");
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,29 @@
-describe('leapYear', function() {
-    it("is false for a year that is not divisible by 4", function() {
-        expect(leapYear(1999)).to.equal(false);
+describe('queenAttack', function() {
+    it("is false if the coordianted are not horizontally, vertically, diagonally in line with each other", function() {
+        expect(queenAttack([1,1], [2,3])).to.equal(false);
     });
+});
 
-    it("is true for most years divisible by 4", function() {
-        expect(leapYear(2012)).to.equal(true);
+describe('queenAttack', function() {
+    it("is true if the coordinates are horizontally or vertically in line with eachother", function() {
+        expect(queenAttack([1,2], [3,2])).to.equal(true);
     });
+});
 
-    it("is false for most years divisble by 100", function() {
-        expect(leapYear(1900)).to.equal(false);
+describe('queenAttack', function() {
+    it("is true if the coordinates are diagonally in line with eachother", function() {
+        expect(queenAttack([3,8], [5,6])).to.equal(true);
     });
+});
 
-    it("is true for most years to be divisible by 400", function() {
-        expect(leapYear(1200)).to.equal(true);
+describe('queenAttack', function() {
+    it("is true if the coordinates are diagonally in line with eachother", function() {
+        expect(queenAttack([1,3], [6,8])).to.equal(true);
+    });
+});
+
+describe('queenAttack', function() {
+    it("is false if the coordinates are not in line in any fashion", function() {
+        expect(queenAttack([1,1], [4,2])).to.equal(false);
     });
 });
